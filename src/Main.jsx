7575d8218a23faa0b1f331ee5e130b80669abc1d
KrishKalavadia <krish55kalavadia@gmail.com>
2026-02-04 16:55:37 +0530

    Fix build: Node version & clean dependencies

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Moog from './components/Moog'
-import Mung from './components/Mung'
 import Til from './components/Til'
 
 function Main() {
---
+++ src/components/Mung.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mung() {
+  return (
+    <div>
+      <img src="/images/mung.png" alt="Mung" style={{display: 'block', margin: 'auto'}}/>
+    </div>
+  )
+}
+
+export default Mung
